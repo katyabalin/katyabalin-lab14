08775ca8042fa422d17f35d1630d6a33210b209c
katyabalin <katya.balin@gmail.com>
2025-04-13 12:45:49 -0400

    set up the basic server class and constructor

--- Server.java
+++ Server.java
@@ -1,1 +1,21 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+public class Server {
+    static final String key = "12345";
+    private ServerSocket SocketServer = null;
+    private ArrayList<LocalDateTime> times;
+
+    public Server(int port) {
+        try {
+            SocketServer = new ServerSocket(port);
+        } catch (Exception e) {
+            System.err.print("couldn't open server socket");
+            e.printStackTrace();
+        }
+        times = new ArrayList<LocalDateTime>();
+    }
+}
 
