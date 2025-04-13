e04964c08201491b382cbea27e6c1968a3d82853
katyabalin <katya.balin@gmail.com>
2025-04-13 13:00:10 -0400

    set up the basic client class and constructor

--- Client.java
+++ Client.java
@@ -1,1 +1,22 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client extends Thread {
+
+    static final String key = "12345";
+    private Socket SocketClient = null;
+
+    public Client(String host, int port) {
+        try {
+            SocketClient = new Socket(host, port);
+        } catch (Exception e) {
+            System.err.print("couldn't open client socket");
+            e.printStackTrace();
+        }
+    }
+
+    public Socket getSocket() {
+        return SocketClient;
+    }
+}
 
